417dc902fcca317fd5f6bf28d427864cf86412fe
Leandro Moran <42581931+LeanM@users.noreply.github.com>
2024-01-30 02:22:27 -0300

    Setup

---
+++ src/main/java/com/jobder/app/JobderApplication.java
@@ -0,0 +1,13 @@
+package com.jobder.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobderApplication.class, args);
+	}
+
+}
